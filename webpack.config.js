a741de2cf9d9041975159eb4ba34267dbd1fa9c4
ayesha <ayeshamundu582@gmail.com>
2018-11-09 15:44:12 +0530

    dist removed

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
+    path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -29,7 +29,7 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
-    contentBase: './dist',
+    contentBase: './',
     hot: true,
   }
 };
