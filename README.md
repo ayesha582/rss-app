9fff3d1e8841f9089d9728c7a73f86b50986e558
ayesha <ayeshamundu582@gmail.com>
2018-11-09 14:21:07 +0530

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-##to start application
-npm install
-npm start
-
-
--- src/store/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import rssDataReducer from './reducers/reducer-rss-data';
-export const rootReducer = combineReducers({
-    rssDataReducer
-})
